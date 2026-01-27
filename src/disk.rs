46966ecb0a121b58a9573810202990fff0d2d85a
Moritisimor <devMoritisimor@proton.me>
2026-01-27 08:03:56 +0100

    Fixed available space display

--- src/disk.rs
+++ src/disk.rs
@@ -5,7 +5,7 @@ pub fn format_disk_data(d: &Disk) -> String {
     format!(
         "{} {:.2} {} / {:.2} {}",
         d.name().display().yellow(),
-        ((d.total_space() - d.available_space()) as f64 / 100000000.).green(),
+        (d.available_space() as f64 / 1000000000.).green(),
         "GB".purple(),
         (d.total_space() as f64 / 1000000000.).green(),
         "GB".purple()
